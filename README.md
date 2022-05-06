049d090c8b5bb91db9246d709f317859d9fe2808
Eman Concepcion <eman_concepcion@yahoo.com>
2022-05-05 21:05:51 -0700

    adding screenshot and readme file

--- README.md
+++ README.md
@@ -4,13 +4,16 @@
 
 i-can-take-notes is an application that helps users keep track of their daily tasks by adding notes into a to do list. When the user is finished with a certain things, as well as being able to remove it from the list.
 
-## Installation 
+## Installation
 
 NONE
 
 ## Screenshots
 
+![](./images/screenshot1.PNG)
+![](./images/screenshot2.PNG)
+
 ## Link
 
-* https://github.com/Emanconcepcion/i-can-take-notes
-* https://emanconcepcion.github.io/i-can-take-notes/
+- https://github.com/Emanconcepcion/i-can-take-notes
+- https://emanconcepcion.github.io/i-can-take-notes/
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -10,15 +10,15 @@ if (window.location.pathname === "/notes") {
   newNoteBtn = document.querySelector(".new-note");
   noteList = document.querySelectorAll(".list-container .list-group");
 }
-// Show an element
+
 const show = (elem) => {
   elem.style.display = "inline";
 };
-// Hide an element
+
 const hide = (elem) => {
   elem.style.display = "none";
 };
-// activeNote is used to keep track of the note in the textarea
+
 let activeNote = {};
 const getNotes = () =>
   fetch("/api/notes", {
@@ -66,9 +66,8 @@ const handleNoteSave = () => {
     renderActiveNote();
   });
 };
-// Delete the clicked note
+
 const handleNoteDelete = (e) => {
-  // Prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
   const note = e.target;
   const noteId = JSON.parse(note.parentElement.getAttribute("data-note")).id;
@@ -80,13 +79,13 @@ const handleNoteDelete = (e) => {
     renderActiveNote();
   });
 };
-// Sets the activeNote and displays it
+
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute("data-note"));
   renderActiveNote();
 };
-// Sets the activeNote to and empty object and allows the user to enter a new note
+
 const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
@@ -98,14 +97,14 @@ const handleRenderSaveBtn = () => {
     show(saveNoteBtn);
   }
 };
-// Render the list of note titles
+
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
   if (window.location.pathname === "/notes") {
     noteList.forEach((el) => (el.innerHTML = ""));
   }
   let noteListItems = [];
-  // Returns HTML element with or without a delete button
+
   const createLi = (text, delBtn = true) => {
     const liEl = document.createElement("li");
     liEl.classList.add("list-group-item");
@@ -140,7 +139,7 @@ const renderNoteList = async (notes) => {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
-// Gets notes from the db and renders them to the sidebar
+
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 if (window.location.pathname === "/notes") {
   saveNoteBtn.addEventListener("click", handleNoteSave);
